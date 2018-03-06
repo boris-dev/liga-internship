46a75b4892af99a6eda6c76bd3c46b6efc3d18a4
bshestakov <bshestakov@at-consulting.com>
2018-03-06 18:37:13 +0300

    правка

--- javacore-song-task/src/main/java/ru/liga/App.java
+++ javacore-song-task/src/main/java/ru/liga/App.java
@@ -4,12 +4,33 @@ import ru.liga.songtask.content.Content;
 import ru.liga.songtask.domain.SimpleMidiFile;
 
 /**
- * Hello world!
+ * Всего нот: 15
+ * <p>
+ * Анализ диапазона:
+ * верхняя: E4
+ * нижняя: F3
+ * диапазон: 11
+ * <p>
+ * Анализ длительности нот (мс):
+ * 4285: 10
+ * 2144: 5
+ * <p>
+ * Анализ нот по высоте:
+ * E4: 3
+ * D4: 3
+ * A3: 3
+ * G3: 3
+ * F3: 3
+ * <p>
+ * Анализ интервалов:
+ * 2: 9
+ * 5: 3
+ * 11: 2
  */
 public class App {
     public static void main(String[] args) {
         SimpleMidiFile simpleMidiFile = new SimpleMidiFile(Content.ZOMBIE);
-        System.out.println("Duration(sec): " + simpleMidiFile.durationMs() / 1000);
-        System.out.println("Note count: " + simpleMidiFile.vocalNoteList().size());
+        System.out.println("Количество нот: " + simpleMidiFile.vocalNoteList().size());
+        System.out.println("Длительность (сек): " + simpleMidiFile.durationMs() / 1000);
     }
 }
